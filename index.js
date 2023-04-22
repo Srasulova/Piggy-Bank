d453c759c331b9f9b0523ebf117232f7a39ef788
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-04-21 23:06:10 -0400

    update total balance and progress bar

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var inputAddAmount = document.querySelector(".add-amount");
 var inputAddDetails = document.querySelector(".add-details-text");
 var transactionType = document.querySelector(".select-transaction-type");
 var activityLogTimes = document.querySelectorAll(".current-time-scope");
+var balance = document.querySelector(".balance");
+var amountSpent = document.querySelector(".amount-spent");
+var budgetedAmount = document.querySelector(".budgeted-amount");
+var goalAmount = document.querySelector(".goal-amount");
+var savedAmout = document.querySelector(".saved-amount");
 // get current time for the timestamp in the table
 var currentDate = new Date();
 var currentHours = currentDate.getHours();
@@ -14,62 +19,99 @@ console.log(currentTimeStamp);
 activityLogTimes.forEach(function (activityLogTime) {
     activityLogTime.innerHTML = currentTimeStamp;
 });
-// activity log class
-var ActivityLog = /** @class */ (function () {
-    function ActivityLog(time, amount, details, transaction) {
-        this.time = time;
-        this.amount = amount;
-        this.details = details;
-        this.transaction = transaction;
-    }
-    return ActivityLog;
-}());
-// btnAddActivity!.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   console.log(transactionType!.value);
-//   console.log(inputAddAmount!.value);
-//   console.log(inputAddDetails!.value);
-// });
-var listItems = [
-    new ActivityLog("10:00", 20, "breakfast with friends", 1),
-    new ActivityLog("13:00", 7, "smoothie", 1),
-    new ActivityLog("17:00", 200, "Uber payment", 2),
-];
+// activity log class. was testing the table data
+// class ActivityLog {
+//   time: string;
+//   amount: number;
+//   details: string;
+//   transaction: number;
+//   constructor(
+//     time: string,
+//     amount: number,
+//     details: string,
+//     transaction: number
+//   ) {
+//     this.time = time;
+//     this.amount = amount;
+//     this.details = details;
+//     this.transaction = transaction;
+//   }
+// }
+// let listItems: any = [
+//   new ActivityLog("10:00", 20, "breakfast with friends", 1),
+//   new ActivityLog("13:00", 7, "smoothie", 1),
+//   new ActivityLog("17:00", 200, "Uber payment", 2),
+// ];
 var dailyTransactionsTable = document.getElementById("daily-transactions-list");
+var totalBalance = [];
+// add new daily transaction line with input data to the table
 function addToDailyTransactionsList() {
+    // create new table row and table cells
     var newRow = dailyTransactionsTable.insertRow(-1);
     var newCellTime = newRow.insertCell(0);
     var newCellAmount = newRow.insertCell(1);
     var newCellDetails = newRow.insertCell(2);
     var newCellTransactionType = newRow.insertCell(3);
+    // define current time when the activity is added
     var textTime = document.createTextNode(currentTimeStamp);
     newCellTime.appendChild(textTime);
-    var textAmount = document.createTextNode("$".concat(inputAddAmount.value));
-    newCellAmount.appendChild(textAmount);
-    newCellAmount.classList.add("fw-semibold");
+    // add details for the transaction
     var textDetails = document.createTextNode(inputAddDetails.value);
     newCellDetails.appendChild(textDetails);
+    // define transaction type
     var textTransactionType;
+    var textAmount;
+    // depending on withdrawal or income add negative or pozitive number
+    // to the list and show the type of transaction
     if (transactionType.value == 1) {
         textTransactionType = document.createTextNode("Withdrawal");
         newCellTransactionType.appendChild(textTransactionType);
         newCellTransactionType.style.color = "red";
+        textAmount = document.createTextNode("- $".concat(inputAddAmount.value));
+        newCellAmount.appendChild(textAmount);
+        newCellAmount.style.color = "red";
+        totalBalance.push(Number("-".concat(inputAddAmount.value)));
     }
     else if (transactionType.value == 2) {
         textTransactionType = document.createTextNode("Income");
         newCellTransactionType.appendChild(textTransactionType);
         newCellTransactionType.style.color = "green";
+        textAmount = document.createTextNode("$".concat(inputAddAmount.value));
+        newCellAmount.appendChild(textAmount);
+        newCellAmount.style.color = "green";
+        totalBalance.push(Number(inputAddAmount.value));
     }
-    // let textTransactionType: any =
-    //   transactionType!.value == 1
-    //     ? document.createTextNode("Withdrawal")
-    //     : transactionType!.value == 2
-    //     ? document.createTextNode("Income")
-    //     : null;
-    // newCellTransactionType.appendChild(textTransactionType);
+    newCellAmount.classList.add("fw-semibold");
 }
+// add button to add the input data to the table
 btnAddActivity.addEventListener("click", function (e) {
     e.preventDefault();
     addToDailyTransactionsList();
+    inputAddAmount.value = "";
+    inputAddDetails.value = "";
+    transactionType.selectedIndex = 0;
+    var sum = 0;
+    for (var i = 0; i < totalBalance.length; i++) {
+        sum += totalBalance[i];
+    }
+    balance.innerHTML = "".concat(sum);
+    if (sum < 0) {
+        balance.style.color = "red";
+    }
+    else if (sum > 0) {
+        balance.style.color = "green";
+    }
 });
-// listItems.forEach((listItem:any) => )
+// Savings progress
+function updateProgressBar() {
+    var amountSpentValue = Number(amountSpent.innerHTML.slice(1));
+    var budgetedAmountValue = Number(budgetedAmount.innerHTML.slice(1));
+    var goalAmountValue = Number(goalAmount.innerHTML.slice(1));
+    var savedAmoutValue = Number(budgetedAmountValue - amountSpentValue);
+    var progressPercentage = Number((savedAmoutValue / goalAmountValue) * 100);
+    savedAmout.innerHTML = "$".concat(savedAmoutValue);
+    var progressBar = document.querySelector(".progress-bar");
+    progressBar.style.width = "".concat(progressPercentage, "%");
+    progressBar.textContent = "".concat(progressPercentage, "%");
+}
+updateProgressBar();
--- index.ts
+++ index.ts
@@ -14,6 +14,16 @@ let transactionType: any = document.querySelector<HTMLSelectElement>(
 let activityLogTimes = document.querySelectorAll<HTMLTableCellElement>(
   ".current-time-scope"
 );
+let balance: any = document.querySelector<HTMLSpanElement>(".balance");
+
+let amountSpent: any = document.querySelector<HTMLSpanElement>(".amount-spent");
+
+let budgetedAmount: any =
+  document.querySelector<HTMLSpanElement>(".budgeted-amount");
+
+let goalAmount: any = document.querySelector<HTMLSpanElement>(".goal-amount");
+
+let savedAmout: any = document.querySelector<HTMLSpanElement>(".saved-amount");
 
 // get current time for the timestamp in the table
 let currentDate = new Date();
@@ -27,89 +37,120 @@ activityLogTimes.forEach((activityLogTime) => {
   activityLogTime.innerHTML = currentTimeStamp;
 });
 
-// activity log class
-class ActivityLog {
-  time: string;
-  amount: number;
-  details: string;
-  transaction: number;
+// activity log class. was testing the table data
 
-  constructor(
-    time: string,
-    amount: number,
-    details: string,
-    transaction: number
-  ) {
-    this.time = time;
-    this.amount = amount;
-    this.details = details;
-    this.transaction = transaction;
-  }
-}
+// class ActivityLog {
+//   time: string;
+//   amount: number;
+//   details: string;
+//   transaction: number;
 
-// btnAddActivity!.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   console.log(transactionType!.value);
-//   console.log(inputAddAmount!.value);
-//   console.log(inputAddDetails!.value);
-// });
+//   constructor(
+//     time: string,
+//     amount: number,
+//     details: string,
+//     transaction: number
+//   ) {
+//     this.time = time;
+//     this.amount = amount;
+//     this.details = details;
+//     this.transaction = transaction;
+//   }
+// }
 
-let listItems: any = [
-  new ActivityLog("10:00", 20, "breakfast with friends", 1),
-  new ActivityLog("13:00", 7, "smoothie", 1),
-  new ActivityLog("17:00", 200, "Uber payment", 2),
-];
+// let listItems: any = [
+//   new ActivityLog("10:00", 20, "breakfast with friends", 1),
+//   new ActivityLog("13:00", 7, "smoothie", 1),
+//   new ActivityLog("17:00", 200, "Uber payment", 2),
+// ];
 
 let dailyTransactionsTable = document.getElementById(
   "daily-transactions-list"
 ) as HTMLTableElement;
 
+let totalBalance: number[] = [];
+
+// add new daily transaction line with input data to the table
 function addToDailyTransactionsList() {
+  // create new table row and table cells
   let newRow = dailyTransactionsTable.insertRow(-1);
   let newCellTime = newRow.insertCell(0);
   let newCellAmount = newRow.insertCell(1);
   let newCellDetails = newRow.insertCell(2);
   let newCellTransactionType = newRow.insertCell(3);
 
+  // define current time when the activity is added
   let textTime = document.createTextNode(currentTimeStamp);
   newCellTime.appendChild(textTime);
 
-  let textAmount = document.createTextNode(`$${inputAddAmount!.value}`);
-  newCellAmount.appendChild(textAmount);
-  newCellAmount.classList.add("fw-semibold");
-
+  // add details for the transaction
   let textDetails = document.createTextNode(inputAddDetails!.value);
   newCellDetails.appendChild(textDetails);
 
+  // define transaction type
   let textTransactionType: any;
+  let textAmount: any;
 
+  // depending on withdrawal or income add negative or pozitive number
+  // to the list and show the type of transaction
   if (transactionType!.value == 1) {
     textTransactionType = document.createTextNode("Withdrawal");
     newCellTransactionType.appendChild(textTransactionType);
     newCellTransactionType.style.color = "red";
+    textAmount = document.createTextNode(`- $${inputAddAmount!.value}`);
+    newCellAmount.appendChild(textAmount);
+    newCellAmount.style.color = "red";
+    totalBalance.push(Number(`-${inputAddAmount!.value}`));
   } else if (transactionType!.value == 2) {
     textTransactionType = document.createTextNode("Income");
     newCellTransactionType.appendChild(textTransactionType);
     newCellTransactionType.style.color = "green";
+    textAmount = document.createTextNode(`$${inputAddAmount!.value}`);
+    newCellAmount.appendChild(textAmount);
+    newCellAmount.style.color = "green";
+    totalBalance.push(Number(inputAddAmount!.value));
   }
-
-  // let textTransactionType: any =
-  //   transactionType!.value == 1
-  //     ? document.createTextNode("Withdrawal")
-  //     : transactionType!.value == 2
-  //     ? document.createTextNode("Income")
-  //     : null;
-
-  // newCellTransactionType.appendChild(textTransactionType);
+  newCellAmount.classList.add("fw-semibold");
 }
 
+// add button to add the input data to the table
 btnAddActivity!.addEventListener("click", (e) => {
   e.preventDefault();
   addToDailyTransactionsList();
+  inputAddAmount!.value = "";
+  inputAddDetails!.value = "";
+  transactionType.selectedIndex = 0;
+
+  let sum: number = 0;
+  for (let i = 0; i < totalBalance.length; i++) {
+    sum += totalBalance[i];
+  }
+
+  balance.innerHTML = `${sum}`;
+
+  if (sum < 0) {
+    balance.style.color = "red";
+  } else if (sum > 0) {
+    balance.style.color = "green";
+  }
 });
 
-// listItems.forEach((listItem:any) => )
+// Savings progress
 
-// to be done:
-// 1. Balance
-// 2. Change color of the amounts and add - if withdrawal
+function updateProgressBar() {
+  let amountSpentValue: number = Number(amountSpent.innerHTML.slice(1));
+  let budgetedAmountValue: number = Number(budgetedAmount.innerHTML.slice(1));
+  let goalAmountValue: number = Number(goalAmount.innerHTML.slice(1));
+  let savedAmoutValue: number = Number(budgetedAmountValue - amountSpentValue);
+
+  let progressPercentage: number = Number(
+    (savedAmoutValue / goalAmountValue) * 100
+  );
+
+  savedAmout.innerHTML = `$${savedAmoutValue}`;
+
+  let progressBar = document.querySelector<HTMLDivElement>(".progress-bar");
+  progressBar!.style.width = `${progressPercentage}%`;
+  progressBar!.textContent = `${progressPercentage}%`;
+}
+updateProgressBar();
